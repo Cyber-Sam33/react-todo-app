e4e7356e581e0fdc61dbd93fc4ee3142ae663e20
Cyber-Sam33 <issasammi@yahoo.com>
2023-04-18 10:23:54 -0400

    Add new todo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
-      <h1>Today's Todos!</h1>
-      <TodoForm />
+      <TodoList />
     </div>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
 
 function TodoList() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    console.log(todo);
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+    setTodos([...todos, todo]);
+    console.log("New todo", todo);
+    console.log("Array of todos", todos);
+  };
+
   return (
-    <div>TodoList</div>
-  )
+    <>
+      <h1>Today's Todos</h1>
+      <TodoForm addTodo={addTodo} />
+    </>
+  );
 }
 
-export default TodoList
+export default TodoList;
